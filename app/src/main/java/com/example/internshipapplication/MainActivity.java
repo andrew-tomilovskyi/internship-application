72844ae05dde8d22fb8767f1b8255a1f3bb16b29
andrew-tomilovskyi <andrex2091@gmail.com>
2020-07-17 11:42:19 +0300

    perda commit

--- app/src/main/java/com/example/internshipapplication/MainActivity.java
+++ app/src/main/java/com/example/internshipapplication/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.internshipapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatEditText;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.internshipapplication.MESSAGE";
+    public static final int SECOND_ACTIVITY_REQUEST_CODE = 42;
+
+    AppCompatEditText input;
+    AppCompatButton btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input = (AppCompatEditText) findViewById(R.id.input);
+        btn = (AppCompatButton) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                String message = Objects.requireNonNull(input.getText()).toString();
+
+                if (TextUtils.isEmpty(input.getText().toString())) {
+                    Toast.makeText(getApplicationContext(), R.string.toast_empty_field, Toast.LENGTH_LONG).show();
+                } else {
+                    intent.putExtra(EXTRA_MESSAGE, message);
+                    startActivityForResult(intent, SECOND_ACTIVITY_REQUEST_CODE);
+                }
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SECOND_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(getApplicationContext(), R.string.toast_result_success, Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Objects.requireNonNull(input.getText()).clear();
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/internshipapplication/SecondActivity.java
@@ -0,0 +1,43 @@
+package com.example.internshipapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+
+
+        AppCompatButton btnOk = (AppCompatButton) findViewById(R.id.btn_ok);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+            }
+        });
+
+        AppCompatButton btnCancel = (AppCompatButton) findViewById(R.id.btn_cancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                setResult(RESULT_CANCELED, intent);
+            }
+        });
+    }
+}
